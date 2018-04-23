865f54de1c68115df8c6dc37d37d355c74699a5f
codepfleger <mail@codepfleger.de>
2018-04-23 19:15:35 +0200

    test

--- trasier-client-api/src/test/java/com/trasier/client/model/SpanTest.java
+++ trasier-client-api/src/test/java/com/trasier/client/model/SpanTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SpanTest {
-
     @Test
     public void shoudlCheckMandatoryFields() {
         assertTrue(isThrowingException(Span.newSpan(null, null, null, null)));
@@ -19,34 +18,6 @@ public class SpanTest {
         ));
     }
 
-//    @Test
-//    public void shouldPopulateFieldsFromProducer() {
-//        // given
-//        UUID conversationId = UUID.randomUUID();
-//        UUID correlationId = UUID.randomUUID();
-//        Span.Builder requestBuilder = Span.newRequestSpan(conversationId, new Application("TestProducer"), "TestOp")
-//                .correlationId(correlationId)
-//                .consumer(new Application("TestConsumer"));
-//        Span request = requestBuilder.build();
-//
-//        // when
-//        Span response = Span.newResponseSpan(requestBuilder).build();
-//
-//        // then
-//        assertEquals(SpanType.REQUEST, request.getType());
-//        assertEquals(SpanType.RESPONSE, response.getType());
-//        assertFalse(request.getError());
-//        assertFalse(response.getError());
-//        assertTrue(response.getProcessingTime() > 0);
-//        assertEquals(0, request.getProcessingTime().intValue());
-//        assertEquals(conversationId, response.getConversationId());
-//        assertEquals(correlationId, response.getCorrelationId());
-//        assertEquals("TestProducer", response.getProducer().getName());
-//        assertEquals("TestConsumer", response.getConsumer().getName());
-//        assertNotNull(request.getTimestamp());
-//        assertNotNull(response.getTimestamp());
-//    }
-
     private boolean isThrowingException(Span.Builder builder) {
         try {
             builder.build();
@@ -55,5 +26,4 @@ public class SpanTest {
             return true;
         }
     }
-
 }
