f41e55cf11878a4005ad45b63d3c8674991e713a
codepfleger <mail@codepfleger.de>
2018-06-12 19:48:34 +0200

    sleuth - wip

---
+++ trasier-client-spring4/src/test/java/com/trasier/client/impl/spring4/OAuthTokenTest.java
@@ -0,0 +1,34 @@
+package com.trasier.client.impl.spring4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class OAuthTokenTest {
+    @Test
+    public void getAccessToken() {
+        OAuthToken token = new OAuthToken();
+        token.setAccessToken("at");
+        assertEquals("at", token.getAccessToken());
+    }
+
+    @Test
+    public void getExpiresIn() {
+        OAuthToken token = new OAuthToken();
+        token.setExpiresIn("1");
+        assertEquals("1", token.getExpiresIn());
+    }
+    @Test
+    public void getRefreshToken() {
+        OAuthToken token = new OAuthToken();
+        token.setRefreshToken("rt");
+        assertEquals("rt", token.getRefreshToken());
+    }
+
+    @Test
+    public void getRefreshExpiresIn() {
+        OAuthToken token = new OAuthToken();
+        token.setExpiresIn("2");
+        assertEquals("2", token.getExpiresIn());
+    }
+}
