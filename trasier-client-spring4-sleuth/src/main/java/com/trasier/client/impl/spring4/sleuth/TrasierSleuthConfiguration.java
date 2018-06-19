ca18637824c67868c3411d05db1ee35af0729c06
codepfleger <mail@codepfleger.de>
2018-06-19 11:19:26 +0200

    review

--- trasier-client-spring4-sleuth/src/main/java/com/trasier/client/impl/spring4/sleuth/TrasierSleuthConfiguration.java
+++ trasier-client-spring4-sleuth/src/main/java/com/trasier/client/impl/spring4/sleuth/TrasierSleuthConfiguration.java
@@ -2,14 +2,16 @@ package com.trasier.client.impl.spring4.sleuth;
 
 import com.trasier.client.impl.spring4.TrasierSpringClientConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackageClasses = TrasierSleuthConfiguration.class)
 public class TrasierSleuthConfiguration {
+    @Bean
     @ConfigurationProperties("trasier.client")
-    public TrasierSpringClientConfiguration createTrasierSpringClientConfiguration() {
+    public TrasierSpringClientConfiguration trasierSpringClientConfiguration() {
         return new TrasierSpringClientConfiguration();
     }
 }
