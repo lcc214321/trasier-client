0d6052b318ffc8f46bc20dbfa2e79799c345dda8
codepfleger <mail@codepfleger.de>
2018-06-20 18:53:39 +0200

    tests

--- trasier-client-spring4/src/main/java/com/trasier/client/impl/spring4/SpringRestClient.java
+++ trasier-client-spring4/src/main/java/com/trasier/client/impl/spring4/SpringRestClient.java
@@ -20,21 +20,30 @@ import java.util.List;
 
 @Component
 public class SpringRestClient implements Client {
-    private final TrasierApplicationConfiguration appConfig;
-
+    private final TrasierApplicationConfiguration applicationConfiguration;
+    private final TrasierSpringClientConfiguration clientConfiguration;
     private final RestTemplate restTemplate;
     private final OAuthTokenSafe tokenSafe;
 
     @Autowired
-    public SpringRestClient(TrasierApplicationConfiguration appConfig, @Qualifier("trasierRestTemplate") RestTemplate restTemplate, OAuthTokenSafe tokenSafe) {
-        this.appConfig = appConfig;
+    public SpringRestClient(TrasierApplicationConfiguration applicationConfiguration, TrasierSpringClientConfiguration clientConfiguration, @Qualifier("trasierRestTemplate") RestTemplate restTemplate, OAuthTokenSafe tokenSafe) {
+        this.applicationConfiguration = applicationConfiguration;
+        this.clientConfiguration = clientConfiguration;
         this.restTemplate = restTemplate;
         this.tokenSafe = tokenSafe;
+    }
+
+    public boolean sendSpan(Span span) {
+        return this.sendSpan(clientConfiguration.getAccountId(), clientConfiguration.getSpaceKey(), span);
     }
 
     @Override
     public boolean sendSpan(String accountId, String spaceKey, Span span) {
         return this.sendSpans(accountId, spaceKey, Collections.singletonList(span));
+    }
+
+    public boolean sendSpans(List<Span> spans) {
+        return this.sendSpans(clientConfiguration.getAccountId(), clientConfiguration.getSpaceKey(), spans);
     }
 
     @Override
@@ -43,7 +52,7 @@ public class SpringRestClient implements Client {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.add("Authorization", "Bearer " + tokenSafe.getAuthHeader());
 
-        UriComponents builder = UriComponentsBuilder.fromHttpUrl(appConfig.getWriterEndpoint()).buildAndExpand(accountId, spaceKey);
+        UriComponents builder = UriComponentsBuilder.fromHttpUrl(applicationConfiguration.getWriterEndpoint()).buildAndExpand(accountId, spaceKey);
         HttpEntity<List<Span>> requestEntity = new HttpEntity<>(spans, headers);
         ResponseEntity<Void> exchange = restTemplate.exchange(builder.toUriString(), HttpMethod.POST, requestEntity, Void.class);
         return !exchange.getStatusCode().is4xxClientError() && !exchange.getStatusCode().is5xxServerError();
--- trasier-client-spring4/src/test/java/com/trasier/client/impl/spring4/SpringRestClientTest.java
+++ trasier-client-spring4/src/test/java/com/trasier/client/impl/spring4/SpringRestClientTest.java
@@ -3,6 +3,7 @@ package com.trasier.client.impl.spring4;
 import com.trasier.client.configuration.TrasierApplicationConfiguration;
 import com.trasier.client.model.Endpoint;
 import com.trasier.client.model.Span;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,26 +21,36 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class SpringRestClientTest {
+    private ResponseEntity<Void> responseEntity;
+    private OAuthTokenSafe oAuthTokenSafe;
+    private TrasierSpringClientConfiguration clientConfiguration;
+
+    @Before
+    public void setup() {
+        this.responseEntity = new ResponseEntity<>(HttpStatus.ACCEPTED);
+        this.oAuthTokenSafe = mock(OAuthTokenSafe.class);
+        this.clientConfiguration = new TrasierSpringClientConfiguration();
+
+        clientConfiguration.setAccountId("170520");
+        clientConfiguration.setSpaceKey("test-1");
+    }
 
     @Test
-    public void testSend() {
-        // given
+    public void testSendSpans() {
         RestTemplate restTemplate = mock(RestTemplate.class);
-        ResponseEntity<Void> responseEntity = new ResponseEntity<>(HttpStatus.ACCEPTED);
         when(restTemplate.exchange(anyString(), any(), any(), eq(Void.class))).thenReturn(responseEntity);
-        OAuthTokenSafe oAuthTokenSafe = mock(OAuthTokenSafe.class);
 
-        SpringRestClient sut = new SpringRestClient(new TrasierApplicationConfiguration(), restTemplate,  oAuthTokenSafe);
+        // given
+        SpringRestClient sut = new SpringRestClient(new TrasierApplicationConfiguration(), clientConfiguration, restTemplate,  oAuthTokenSafe);
 
         // when
         Span span = Span.newSpan(UUID.randomUUID().toString(), UUID.randomUUID().toString(), new Endpoint("Test1"), "TEST")
                 .outgoingEndpoint(new Endpoint("Test2")).startTimestamp(1L).build();
 
-        boolean result = sut.sendSpans("170520", "test-1", Collections.singletonList(span));
+        boolean result = sut.sendSpans(Collections.singletonList(span));
 
         // then
         assertTrue(result);
         verify(restTemplate);
     }
-
 }
