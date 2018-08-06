7465ea23f1a5e18b55e947a64e25155cddf4ae81
codepfleger <mail@codepfleger.de>
2018-08-06 13:53:23 +0200

    context

--- trasier-client-api/src/main/java/com/trasier/client/model/Endpoint.java
+++ trasier-client-api/src/main/java/com/trasier/client/model/Endpoint.java
@@ -18,37 +18,4 @@ public class Endpoint {
     public Endpoint(String name) {
         this.name = name;
     }
-
-    public Endpoint(String name, String ipAddress, String port, String hostname) {
-        this.name = name;
-        this.ipAddress = ipAddress;
-        this.port = port;
-        this.hostname = hostname;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getIpAddress() {
-        return ipAddress;
-    }
-
-    public String getPort() {
-        return port;
-    }
-
-    public String getHostname() {
-        return hostname;
-    }
-
-    @Override
-    public String toString() {
-        return "Endpoint{" +
-                "name='" + name + '\'' +
-                ", ipAddress='" + ipAddress + '\'' +
-                ", port='" + port + '\'' +
-                ", hostname='" + hostname + '\'' +
-                '}';
-    }
 }
