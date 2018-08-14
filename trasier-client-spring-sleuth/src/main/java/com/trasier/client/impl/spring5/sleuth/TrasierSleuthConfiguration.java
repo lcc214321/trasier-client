c044785ce2b5f4d10b3734f0edd3bc3830a3b6bc
codepfleger <mail@codepfleger.de>
2018-08-14 19:02:44 +0200

    spring5

---
+++ trasier-client-spring-sleuth/src/main/java/com/trasier/client/impl/spring5/sleuth/TrasierSleuthConfiguration.java
@@ -0,0 +1,24 @@
+package com.trasier.client.impl.spring5.sleuth;
+
+import com.trasier.client.configuration.TrasierEndpointConfiguration;
+import com.trasier.client.configuration.TrasierClientConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = TrasierSleuthConfiguration.class)
+public class TrasierSleuthConfiguration {
+    @Bean
+    @ConfigurationProperties("trasier.client")
+    public TrasierClientConfiguration trasierSpringClientConfiguration() {
+        return new TrasierClientConfiguration();
+    }
+
+    @Bean
+    @ConfigurationProperties("trasier.endpoint")
+    protected TrasierEndpointConfiguration endpointConfig() {
+        return new TrasierEndpointConfiguration();
+    }
+}
