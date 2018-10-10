d29d349dc9ba1894a9463710afb25dedc0e1bdd3
Lukasz Pielak <pielak@gmail.com>
2018-10-10 16:17:47 +0100

    getting rid of gcp pubsub client

--- trasier-client-pubsub/src/main/java/com/trasier/client/impl/pubsub/CompressionMimeType.java
+++
@@ -1,6 +0,0 @@
-package com.trasier.client.impl.pubsub;
-
-class CompressionMimeType {
-    static final String NONE = "application/octet-stream";
-    static final String SNAPPY = "application/x-snappy-framed";
-}
