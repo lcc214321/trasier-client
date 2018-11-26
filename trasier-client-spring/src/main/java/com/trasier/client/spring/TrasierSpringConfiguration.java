cc4319af00d352b22c31f172e175e49735852deb
Lukasz Pielak <pielak@gmail.com>
2018-11-26 18:07:24 +0100

    increase default Q size

--- trasier-client-spring/src/main/java/com/trasier/client/spring/TrasierSpringConfiguration.java
+++ trasier-client-spring/src/main/java/com/trasier/client/spring/TrasierSpringConfiguration.java
@@ -1,16 +1,17 @@
 package com.trasier.client.spring;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 import com.trasier.client.spring.auth.OAuthToken;
 import com.trasier.client.spring.client.TrasierSpringClient;
 import com.trasier.client.spring.context.TrasierSpringAccessor;
 import com.trasier.client.spring.spancontrol.TrasierSampleByOperationInterceptor;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackageClasses = {OAuthToken.class, TrasierSpringClient.class, TrasierSpringAccessor.class, TrasierSampleByOperationInterceptor.class})
 public class TrasierSpringConfiguration {
-    private int queueSize = 100;
+    private int queueSize = 1000;
     private int queueSizeErrorThresholdMultiplicator = 10;
     private long queueDelay = 1000L;
     private int maxTaskCount = 100;
