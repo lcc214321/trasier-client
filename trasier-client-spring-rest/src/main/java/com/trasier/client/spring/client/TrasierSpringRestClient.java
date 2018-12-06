8ca258f10b19e0016e2ac13d69d41cd56f771f67
codepfleger <mail@codepfleger.de>
2018-12-06 20:39:35 +0100

    ssl and grpc prepared

--- trasier-client-spring-rest/src/main/java/com/trasier/client/spring/client/TrasierSpringRestClient.java
+++ trasier-client-spring-rest/src/main/java/com/trasier/client/spring/client/TrasierSpringRestClient.java
@@ -5,6 +5,9 @@ import com.trasier.client.configuration.TrasierClientConfiguration;
 import com.trasier.client.configuration.TrasierEndpointConfiguration;
 import com.trasier.client.interceptor.TrasierSpanInterceptor;
 import com.trasier.client.spring.auth.OAuthTokenSafe;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +16,19 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -36,8 +46,8 @@ public class TrasierSpringRestClient implements TrasierSpringClient {
     private final List<TrasierSpanInterceptor> spanInterceptors = new ArrayList<>();
 
     @Autowired
-    public TrasierSpringRestClient(TrasierEndpointConfiguration applicationConfiguration, TrasierClientConfiguration clientConfiguration, OAuthTokenSafe tokenSafe) {
-        this(applicationConfiguration, clientConfiguration, new RestTemplate(), tokenSafe);
+    public TrasierSpringRestClient(TrasierEndpointConfiguration applicationConfiguration, TrasierClientConfiguration clientConfiguration, OAuthTokenSafe tokenSafe) throws Exception {
+        this(applicationConfiguration, clientConfiguration, createRestTemplate(), tokenSafe);
     }
 
     TrasierSpringRestClient(TrasierEndpointConfiguration applicationConfiguration, TrasierClientConfiguration clientConfiguration, RestTemplate restTemplate, OAuthTokenSafe tokenSafe) {
@@ -84,14 +94,14 @@ public class TrasierSpringRestClient implements TrasierSpringClient {
             HttpEntity<List<Span>> requestEntity = new HttpEntity<>(spans, headers);
             ResponseEntity<Void> exchange = restTemplate.exchange(builder.toUriString(), HttpMethod.POST, requestEntity, Void.class);
             return !exchange.getStatusCode().is4xxClientError() && !exchange.getStatusCode().is5xxServerError();
-        } catch(Exception e) {
+        } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
             return false;
         }
     }
 
     private void applyInterceptors(List<Span> spans) {
-        spans.removeIf(span ->  {
+        spans.removeIf(span -> {
             applyInterceptors(span);
             return span.isCancel();
         });
@@ -106,6 +116,26 @@ public class TrasierSpringRestClient implements TrasierSpringClient {
     @Override
     public void close() {
         // do nothing
+    }
+
+
+    private static RestTemplate createRestTemplate() throws Exception {
+        char[] password = "trasier".toCharArray();
+        SSLContext sslContext = new SSLContextBuilder()
+                .loadKeyMaterial(keyStore("classpath:trasier.jks", password), password)
+                .build();
+        HttpClient client = HttpClients.custom().setSSLContext(sslContext).build();
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(client));
+    }
+
+
+    private static KeyStore keyStore(String file, char[] password) throws Exception {
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        File key = ResourceUtils.getFile(file);
+        try (InputStream in = new FileInputStream(key)) {
+            keyStore.load(in, password);
+        }
+        return keyStore;
     }
 
 }
