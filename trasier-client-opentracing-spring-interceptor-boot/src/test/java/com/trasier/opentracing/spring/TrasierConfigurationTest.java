9ff6f8f9a837a40fa81207ca268d03f59e8f3afc
Lukasz Pielak <pielak@gmail.com>
2019-03-29 18:46:26 +0100

    Feature/lp int tests (#64)
    * integration tests

---
+++ trasier-client-opentracing-spring-interceptor-boot/src/test/java/com/trasier/opentracing/spring/TrasierConfigurationTest.java
@@ -0,0 +1,52 @@
+package com.trasier.opentracing.spring;
+
+import com.trasier.client.configuration.TrasierClientConfiguration;
+import com.trasier.client.configuration.TrasierEndpointConfiguration;
+import com.trasier.client.opentracing.TrasierTracer;
+import com.trasier.client.opentracing.spring.boot.TrasierOpentracingConfiguration;
+import com.trasier.client.spring.TrasierSpringClientQueueConfiguration;
+import com.trasier.client.spring.spancontrol.TrasierSampleByOperationConfiguration;
+import com.trasier.client.spring.spancontrol.TrasierSampleByUrlPatternConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@Import(TrasierOpentracingConfiguration.class)
+public class TrasierConfigurationTest {
+
+    @Autowired
+    private TrasierSpringClientQueueConfiguration trasierSpringClientQueueConfiguration;
+
+    @Autowired
+    private TrasierClientConfiguration trasierSpringClientConfiguration;
+
+    @Autowired
+    private TrasierEndpointConfiguration endpointConfig;
+
+    @Autowired
+    private TrasierTracer trasierTracer;
+
+    @Autowired
+    private TrasierSampleByOperationConfiguration samplingFilterOperationConfiguration;
+
+    @Autowired
+    private TrasierSampleByUrlPatternConfiguration samplingFilterUrlConfiguration;
+
+    @Test
+    public void testAutoconfigWorks() {
+        assertNotNull(trasierSpringClientConfiguration);
+        assertNotNull(endpointConfig);
+        assertNotNull(trasierTracer);
+        assertNotNull(samplingFilterOperationConfiguration);
+        assertNotNull(samplingFilterUrlConfiguration);
+        assertNotNull(trasierSpringClientQueueConfiguration);
+        assertTrue(trasierSpringClientQueueConfiguration.getQueueSize() > 0);
+    }
+
+}
