ec1220f8eaa072cda567fc2c821d06a6d8e7e48a
Lukasz Pielak <pielak@gmail.com>
2020-05-26 22:46:54 +0200

    improve span filter interceptors. Allow to disable payload bye url or operation name

--- trasier-client-core/src/main/java/com/trasier/client/configuration/TrasierFilterConfiguration.java
+++ trasier-client-core/src/main/java/com/trasier/client/configuration/TrasierFilterConfiguration.java
@@ -3,6 +3,7 @@ package com.trasier.client.configuration;
 import lombok.Data;
 
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 @Data
 public class TrasierFilterConfiguration {
@@ -11,11 +12,22 @@ public class TrasierFilterConfiguration {
     private Pattern operation;
 
     public void setUrl(String urlPattern) {
-        this.url = Pattern.compile(urlPattern);
+        this.url = toPattern(urlPattern);
     }
 
     public void setOperation(String operationPattern) {
-        this.operation = Pattern.compile(operationPattern);
+        this.operation = toPattern(operationPattern);
+    }
+
+    private Pattern toPattern(String pattern) {
+        if (pattern == null) {
+            return null;
+        }
+        try {
+            return Pattern.compile(pattern);
+        } catch (PatternSyntaxException e) {
+            throw new IllegalArgumentException("Invalid filter pattern " + pattern, e);
+        }
     }
 
 }
